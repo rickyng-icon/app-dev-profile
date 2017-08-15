3946b5aad5509cc9d9c8611b6257705a7660ceed
Ng Ricky <rkhome@Ng-Rickyteki-MacBook-Air.local>
2017-08-15 14:01:47 +0800

    initial xcode project

---
+++ dev-profile/dev-profile/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  dev-profile
+//
+//  Created by Ng Ricky on 15/8/2017.
+//  Copyright © 2017 ICON. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
